bcc19e0d3fde5d964594e6ec1ff120dd45cef5d1
Umbrason <FabioKolligs@outlook.de>
2022-02-20 21:54:17 +0100

    fixed some null refs and added dynamic mesh merging support

--- Scripts/DynamicMesh.cs
+++ Scripts/DynamicMesh.cs
@@ -9,36 +9,35 @@ namespace ProceduralMeshes
     {
         public string name;
         private List<Vector3> vertices;
-        public List<Vector3> Vertices { get { if (vertices == null) vertices = new List<Vector3>(); return vertices; } set { vertices = value; } }
+        public List<Vector3> Vertices { get { return vertices ??= new List<Vector3>(); } set { vertices = value; } }
         private List<int> indices;
-        public List<int> Indices { get { if (indices == null) indices = new List<int>(); return indices; } set { indices = value; } }
+        public List<int> Indices { get { return indices ??= new List<int>(); } set { indices = value; } }
         private List<Vector3> normals;
-        public List<Vector3> Normals { get { if (normals == null) normals = new List<Vector3>(); return normals; } set { normals = value; } }
+        public List<Vector3> Normals { get { return normals ??= new List<Vector3>(); } set { normals = value; } }
 
         private List<Color> colors;
-        public List<Color> Colors { get { if (colors == null) colors = new List<Color>(); return colors; } set { colors = value; } }
+        public List<Color> Colors { get { return colors ??= new List<Color>(); } set { colors = value; } }
 
         private List<Vector2> uv0;
-        public List<Vector2> UV0 { get { if (uv0 == null) uv0 = new List<Vector2>(); return uv0; } set { uv0 = value; } }
+        public List<Vector2> UV0 { get { return uv0 ??= new List<Vector2>(); } set { uv0 = value; } }
 
         private List<Vector2> uv1;
-        public List<Vector2> UV1 { get { if (uv1 == null) uv1 = new List<Vector2>(); return uv1; } set { uv1 = value; } }
+        public List<Vector2> UV1 { get { return uv1 ??= new List<Vector2>(); } set { uv1 = value; } }
 
         private List<Vector2> uv2;
-        public List<Vector2> UV2 { get { if (uv2 == null) uv2 = new List<Vector2>(); return uv2; } set { uv2 = value; } }
-
+        public List<Vector2> UV2 { get { return uv2 ??= new List<Vector2>(); } set { uv2 = value; } }
 
         private List<Vector2> uv3;
-        public List<Vector2> UV3 { get { if (uv3 == null) uv3 = new List<Vector2>(); return uv3; } set { uv3 = value; } }
+        public List<Vector2> UV3 { get { return uv3 ??= new List<Vector2>(); } set { uv3 = value; } }
 
         private List<Vector2> uv4;
-        public List<Vector2> UV4 { get { if (uv4 == null) uv4 = new List<Vector2>(); return uv4; } set { uv4 = value; } }
+        public List<Vector2> UV4 { get { return uv4 ??= uv4 = new List<Vector2>(); } set { uv4 = value; } }
 
         private List<BoneWeight> boneWeights;
-        public List<BoneWeight> BoneWeights { get { if (boneWeights == null) boneWeights = new List<BoneWeight>(); return boneWeights; } set { boneWeights = value; } }
+        public List<BoneWeight> BoneWeights { get { return boneWeights ??= new List<BoneWeight>(); } set { boneWeights = value; } }
 
         private List<Matrix4x4> bindPoses;
-        public List<Matrix4x4> BindPoses { get { if (bindPoses == null) bindPoses = new List<Matrix4x4>(); return bindPoses; } set { bindPoses = value; } }
+        public List<Matrix4x4> BindPoses { get { return bindPoses ??= new List<Matrix4x4>(); ; } set { bindPoses = value; } }
 
         public MeshTopology topology;
 
@@ -67,30 +66,64 @@ namespace ProceduralMeshes
         }
 
         public void AppendMesh(Mesh mesh, Vector3 position, Quaternion rotation, Vector3 scale) => AppendMesh(mesh, Matrix4x4.TRS(position, rotation, scale));
-        public void AppendMesh(Mesh mesh, Matrix4x4 TRS)
+        public void AppendMesh(Mesh mesh, Matrix4x4? TRS)
         {
             for (int s = 0; s < mesh.subMeshCount; s++)
             {
                 UnityEngine.Rendering.SubMeshDescriptor subMeshDesc = mesh.GetSubMesh(s);
+                AppendMesh(subMeshDesc.baseVertex, subMeshDesc.vertexCount, subMeshDesc.indexStart, subMeshDesc.indexCount, TRS ??= Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one),
+                mesh.vertices,
+                mesh.GetIndices(s),
+                mesh.normals,
+                mesh.colors,
+                mesh.uv,
+                mesh.uv2,
+                mesh.uv3,
+                mesh.uv4,
+                mesh.uv5,
+                mesh.boneWeights
+                );
                 if (subMeshDesc.topology != topology)
                     continue;
-
-                int indexOffset = vertices.Count;
-                for (int i = 0; i < subMeshDesc.vertexCount; i++)
-                {
-                    Vertices.Add(TRS * mesh.vertices[i + subMeshDesc.baseVertex]);
-                    Normals.Add(TRS.rotation * mesh.normals[i + subMeshDesc.baseVertex]);
-                    Colors.Add(mesh.colors[i + subMeshDesc.baseVertex]);
-                    uv0.Add(mesh.uv[i]);
-                    uv1.Add(mesh.uv2[i]);
-                    uv2.Add(mesh.uv3[i]);
-                    uv3.Add(mesh.uv4[i]);
-                    uv4.Add(mesh.uv5[i]);
-                    boneWeights.Add(mesh.boneWeights[i]);
-                }
-                for (int i = 0; i < subMeshDesc.indexCount; i++)
-                    Indices.Add(mesh.triangles[i + subMeshDesc.indexStart] + indexOffset);
             }
+        }
+
+        public void AppendMesh(DynamicMesh mesh, Matrix4x4? TRS)
+        {
+            if (this.topology != mesh.topology)
+                return;
+            AppendMesh(0, vertices.Count, 0, indices.Count, TRS ??= Matrix4x4.TRS(Vector3.zero, Quaternion.identity, Vector3.one),
+                mesh.Vertices,
+                mesh.Indices,
+                mesh.Normals,
+                mesh.Colors,
+                mesh.UV0,
+                mesh.UV1,
+                mesh.UV2,
+                mesh.UV3,
+                mesh.UV4,
+                mesh.BoneWeights
+            );
+        }
+
+        public void AppendMesh(int baseVertex, int vertexCount, int baseIndex, int indexCount, Matrix4x4 TRS, IList<Vector3> meshVertices, IList<int> meshIndices, IList<Vector3> meshNormals, IList<Color> meshColors, IList<Vector2> meshUV0, IList<Vector2> meshUV1, IList<Vector2> meshUV2, IList<Vector2> meshUV3, IList<Vector2> meshUV4, IList<BoneWeight> meshBoneWeights)
+        {
+            int indexOffset = vertices.Count;
+            for (int i = 0; i < vertexCount; i++)
+            {
+                var index = i + baseVertex;
+                Vertices.Add(meshVertices?.Count > index ? TRS * meshVertices[index] : default);
+                Normals.Add(meshNormals?.Count > index ? TRS.rotation * meshNormals[index] : default);
+                Colors.Add(meshColors?.Count > index ? meshColors[index] : default);
+                UV0.Add(meshUV0?.Count > index ? meshUV0[index] : default);
+                UV1.Add(meshUV1?.Count > index ? meshUV1[index] : default);
+                UV2.Add(meshUV2?.Count > index ? meshUV2[index] : default);
+                UV3.Add(meshUV3?.Count > index ? meshUV3[index] : default);
+                UV4.Add(meshUV4?.Count > index ? meshUV4[index] : default);
+                BoneWeights.Add(meshBoneWeights?.Count > index ? meshBoneWeights[index] : default);
+            }
+            for (int i = 0; i < indexCount; i++)
+                Indices.Add(meshIndices[i + baseIndex] + indexOffset);
         }
     }
 }
